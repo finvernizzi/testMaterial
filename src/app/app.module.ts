2e3662ae965badcd3318add76b77acf99bc56059
Invernizzi Fabrizio <00918083@telecomitalia.local>
2018-07-05 15:33:56 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MaterialNavComponent } from './material-nav/material-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatMenuModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaterialNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
